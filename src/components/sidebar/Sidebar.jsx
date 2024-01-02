2cb8e511696744a388f00ca0d4324ffabe188969
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-02 12:58:49 -0500

    add variants

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,7 +8,22 @@ const Sidebar = () => {
     const [open, setOpen] = useState(false)
 
     const variants = {
-
+        open: {
+            clipPath: "circle(1200px at 50px 50px)",
+            transition: {
+                type: "spring",
+                stiffness: 20,
+            }
+        },
+        closed: {
+            clipPath: "circle(30px at 50px 50px)",
+            transition: {
+                delay: 0.5,
+                type: "spring",
+                stiffness: 400,
+                damping: 40,
+            }
+        }
     }
 
     return (
