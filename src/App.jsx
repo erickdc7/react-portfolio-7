fca3a6a3544876ba8f7fe1631f0ac00723b8b722
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-01 20:44:24 -0500

    add link

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./app.scss"
 const App = () => {
   return (
     <div>
-      <section>Hero</section>
+      <section>
+        <a href="">Hello</a>
+      </section>
       <section>Parallax</section>
       <section>Services</section>
       <section>Parallax</section>
