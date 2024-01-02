383ff4c4ab96994a1d223defda01795770af1e61
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-02 13:11:20 -0500

    add svg

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,7 +1,11 @@
 const ToggleButton = ({ setOpen }) => {
     return (
         <button onClick={() => setOpen((prev) => !prev)}>
-            button
+            <svg width="23" height="23" viewBox="0 0 23 23">
+                <path strokeWidth="3" stroke="black" strokeLinecap="round" />
+                <path strokeWidth="3" stroke="black" strokeLinecap="round" />
+                <path strokeWidth="3" stroke="black" strokeLinecap="round" />
+            </svg>
         </button>
     )
 }
