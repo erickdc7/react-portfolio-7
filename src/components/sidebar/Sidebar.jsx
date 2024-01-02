cb73265f1d13413b62b08f957e03e7bbc3e274ac
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-02 12:51:09 -0500

    add useState

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
 import Links from "./links/Links"
 import "./sidebar.scss"
 import ToggleButton from "./toggleButton/toggleButton"
 
 const Sidebar = () => {
+    const [open, setOpen] = useState(false)
+
     return (
         <div className="sidebar">
             <div className="bg">
