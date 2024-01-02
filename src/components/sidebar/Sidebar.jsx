1aa0d02e79488c6397515e37349ea0920a129c9b
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-02 12:53:22 -0500

    add framer motion

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,17 +2,18 @@ import { useState } from "react"
 import Links from "./links/Links"
 import "./sidebar.scss"
 import ToggleButton from "./toggleButton/toggleButton"
+import { motion } from "framer-motion"
 
 const Sidebar = () => {
     const [open, setOpen] = useState(false)
 
     return (
-        <div className="sidebar">
+        <motion.div className="sidebar">
             <div className="bg">
                 <Links />
             </div>
             <ToggleButton setOpen={setOpen} />
-        </div>
+        </motion.div>
     )
 }
 
