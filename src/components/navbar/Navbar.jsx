c4dc3198e41deaaa71eb30f1a392e6c3a50c85b4
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-02 12:25:05 -0500

    add links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,18 @@ const Navbar = () => {
       <div className="wrapper">
         <span>Lama Dev</span>
         <div className="social">
-          <a href="#"></a>
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribble.png" alt="" />
+          </a>
         </div>
       </div>
     </div>
