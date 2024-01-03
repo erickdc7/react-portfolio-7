d9a2db0da52e43f2849ae3d3d790330c2db3892b
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-03 12:58:03 -0500

    add text variants

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,20 @@
 import "./hero.scss"
 
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        }
+    }
+}
+
 const Hero = () => {
     return (
         <div className="hero">
