eb9edda5e10a292c83b34e916edf60df8b5d07cf
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-03 12:29:23 -0500

    add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./app.scss"
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     <div>
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
       <section id="Services">Parallax</section>
       <section>Services</section>
