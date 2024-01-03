8d8647eec5522f0de3987c6ddd337d18644b5725
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-03 12:15:31 -0500

    update variants

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,27 +4,27 @@ import "./sidebar.scss"
 import ToggleButton from "./toggleButton/toggleButton"
 import { motion } from "framer-motion"
 
-const Sidebar = () => {
-    const [open, setOpen] = useState(false)
-
-    const variants = {
-        open: {
-            clipPath: "circle(1200px at 50px 50px)",
-            transition: {
-                type: "spring",
-                stiffness: 20,
-            }
-        },
-        closed: {
-            clipPath: "circle(30px at 50px 50px)",
-            transition: {
-                delay: 0.5,
-                type: "spring",
-                stiffness: 400,
-                damping: 40,
-            }
+const variants = {
+    open: {
+        clipPath: "circle(1200px at 50px 50px)",
+        transition: {
+            type: "spring",
+            stiffness: 20,
+        }
+    },
+    closed: {
+        clipPath: "circle(30px at 50px 50px)",
+        transition: {
+            delay: 0.5,
+            type: "spring",
+            stiffness: 400,
+            damping: 40,
         }
     }
+}
+
+const Sidebar = () => {
+    const [open, setOpen] = useState(false)
 
     return (
         <motion.div
