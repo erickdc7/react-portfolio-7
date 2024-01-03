15fec91336a0805f2844b7e0279cd04a356c8805
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-03 12:34:33 -0500

    add title

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,6 +5,7 @@ const Hero = () => {
         <div className="hero">
             <div className="textContainer">
                 <h2>HARVEY TYLER</h2>
+                <h1>Web developer and UI designer</h1>
             </div>
             <div className="imageContainer">
                 <img src="/hero.png" alt="" />
