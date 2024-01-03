3f8d7b8ad48b42bf52a17ee4566e2e82bf3d2ae5
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-03 12:33:41 -0500

    add text container

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,6 +3,9 @@ import "./hero.scss"
 const Hero = () => {
     return (
         <div className="hero">
+            <div className="textContainer">
+
+            </div>
             <div className="imageContainer">
                 <img src="/hero.png" alt="" />
             </div>
