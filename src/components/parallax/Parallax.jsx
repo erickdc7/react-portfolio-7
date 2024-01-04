87d5959cf2d081b1d2d98acbd0299565e60151fb
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-04 12:47:23 -0500

    add container

---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,16 @@
+import "./parallax.scss"
+
+const Parallax = ({ type }) => {
+    return (
+        <div className="parallax">
+            <h1>
+                {type === "services" ? "What We Do?" : "What We Did?"}
+            </h1>
+
+            <div className="mountains"></div>
+            <div className="planets"></div>
+        </div>
+    )
+}
+
+export default Parallax
