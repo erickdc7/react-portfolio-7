6ffe188320eeb642d6a410d4a6a3e40eaf6de264
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-05 12:19:01 -0500

    update yBg

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -10,7 +10,7 @@ const Parallax = ({ type }) => {
         offset: ["start start", "end start"]
     })
 
-    const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
+    const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "500%"])
 
     return (
         <div
