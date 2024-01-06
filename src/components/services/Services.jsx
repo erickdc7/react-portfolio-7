33c0d48eef096dba398ae75c94dd6dbc304f20c3
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-06 13:53:24 -0500

    add variants

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -26,7 +26,7 @@ const Services = () => {
             initial="initial"
             animate="animate"
         >
-            <motion.div className="textContainer">
+            <motion.div className="textContainer" variants={variants}>
                 <p>
                     I focus on helping your brand grow <br />
                     and move forward
@@ -34,7 +34,7 @@ const Services = () => {
                 <hr />
             </motion.div>
 
-            <motion.div className="titleContainer">
+            <motion.div className="titleContainer" variants={variants}>
                 <div className="title">
                     <img src="/people.webp" alt="" />
                     <h1>
@@ -50,7 +50,7 @@ const Services = () => {
                 </div>
             </motion.div>
 
-            <motion.div className="listContainer">
+            <motion.div className="listContainer" variants={variants}>
                 <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
                     <h2>Branding</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus qui odit alias asperiores quibusdam totam consequuntur? Nesciunt velit iste at!</p>
