b84015f542441369d29db52d964580b989ac8750
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-07 13:54:54 -0500

    add items

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,7 +6,25 @@ const items = [
         title: "React Commerce",
         img: "https://images.pexels.com/photos/2181230/pexels-photo-2181230.jpeg?auto=compress&cs=tinysrgb&w=600",
         desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam accusantium perferendis quasi labore cum quia officiis dolor, assumenda atque ipsa!",
-    }
+    },
+    {
+        id: 2,
+        title: "Next.js Blog",
+        img: "https://images.pexels.com/photos/599650/pexels-photo-599650.jpeg?auto=compress&cs=tinysrgb&w=600",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam accusantium perferendis quasi labore cum quia officiis dolor, assumenda atque ipsa!",
+    },
+    {
+        id: 3,
+        title: "Vanilla JS App",
+        img: "https://images.pexels.com/photos/2262628/pexels-photo-2262628.jpeg?auto=compress&cs=tinysrgb&w=600",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam accusantium perferendis quasi labore cum quia officiis dolor, assumenda atque ipsa!",
+    },
+    {
+        id: 4,
+        title: "Music App",
+        img: "https://images.pexels.com/photos/1926701/pexels-photo-1926701.jpeg?auto=compress&cs=tinysrgb&w=600",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam accusantium perferendis quasi labore cum quia officiis dolor, assumenda atque ipsa!",
+    },
 ]
 
 const Portfolio = () => {
