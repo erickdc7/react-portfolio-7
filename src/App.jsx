5ad9bc0fdef5ccc918d928e9379452a304667070
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-07 13:24:20 -0500

    add portfolio component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss"
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
 const App = () => {
@@ -20,9 +21,7 @@ const App = () => {
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
+      <Portfolio />
       <section id="Contact">Contact</section>
     </div>
   );
