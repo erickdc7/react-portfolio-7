288ba90cc8fa1276d0f8f32ec4bcbcc0dd45ca35
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-07 13:51:54 -0500

    add item

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,14 @@
 import "./portfolio.scss"
 
+const items = [
+    {
+        id: 1,
+        title: "React Commerce",
+        img: "https://images.pexels.com/photos/2181230/pexels-photo-2181230.jpeg?auto=compress&cs=tinysrgb&w=600",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam accusantium perferendis quasi labore cum quia officiis dolor, assumenda atque ipsa!",
+    }
+]
+
 const Portfolio = () => {
     return (
         <div className="portfolio">Portfolio</div>
