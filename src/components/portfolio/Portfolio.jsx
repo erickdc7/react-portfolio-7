ca572378fd04f6ed24bb1acb3b6b52f4191e6c5e
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-08 12:09:32 -0500

    update useTransform

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -34,10 +34,10 @@ const Single = ({ item }) => {
 
     const { scrollYProgress } = useScroll({
         target: ref,
-        offset: ["start start", "end start"]
+        //offset: ["start start", "end start"]
     })
 
-    const y = useTransform(scrollYProgress, [0, 1], ["0%", "-300%"])
+    const y = useTransform(scrollYProgress, [0, 1], [-300, 300])
 
     return (
         <section>
