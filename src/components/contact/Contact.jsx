db13a23db025ae51b5e5e64a6c4dfcc9dd9057b8
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-09 12:56:17 -0500

    add title

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,9 @@ import "./contact.scss"
 const Contact = () => {
     return (
         <div className="contact">
-            <div className="textContainer"></div>
+            <div className="textContainer">
+                <h1>Let&apos;s work together</h1>
+            </div>
 
             <div className="formContainer"></div>
         </div>
