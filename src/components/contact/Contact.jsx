d8d6340a1456be26a5edd9ae891c01437b48d5c3
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-09 13:33:23 -0500

    add variants

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,20 @@
 import "./contact.scss"
 
+const variants = {
+    initial: {
+        y: 500,
+        opacity: 0,
+    },
+    animate: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+            staggerChildren: 0.1,
+        }
+    }
+}
+
 const Contact = () => {
     return (
         <div className="contact">
