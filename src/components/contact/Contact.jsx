acc510443f5c59f9f9ccff3a39ee45843cd137ca
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-09 12:56:58 -0500

    add title

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,7 +5,9 @@ const Contact = () => {
         <div className="contact">
             <div className="textContainer">
                 <h1>Let&apos;s work together</h1>
-                <div className="item"></div>
+                <div className="item">
+                    <h2>Mail</h2>
+                </div>
             </div>
 
             <div className="formContainer"></div>
