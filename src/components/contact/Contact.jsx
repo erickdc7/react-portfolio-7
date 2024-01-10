fa9ea9135e9ffffe1fb14288b2cde811ec063467
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-10 11:31:52 -0500

    add variants

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
                     <h2>Address</h2>
                     <span>123 Hello Street New York</span>
                 </motion.div>
-                <motion.div className="item">
+                <motion.div className="item" variants={variants}>
                     <h2>Phone</h2>
                     <span>+1 234 5678</span>
                 </motion.div>
