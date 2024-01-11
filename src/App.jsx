87163807799f157b5302363d1eb0922f3828321c
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-11 12:21:41 -0500

    add cursor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./app.scss"
 import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -26,6 +27,7 @@ const App = () => {
       <section id="Contact">
         <Contact />
       </section>
+      <Cursor />
     </div>
   );
 };
