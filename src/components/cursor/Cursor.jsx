e578e7fc4042467b230204d8492f056654d3d23f
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-11 12:21:11 -0500

    add useEffect

--- src/components/cursor/Cursor.jsx
+++ src/components/cursor/Cursor.jsx
@@ -4,6 +4,18 @@ import { useState, useEffect } from "react"
 const Cursor = () => {
     const [position, setPosition] = useState({ x: 0, y: 0 })
 
+    useEffect(() => {
+        const mouseMove = (e) => {
+            setPosition({ x: e.clientX, y: e.clientY })
+        }
+
+        window.addEventListener("mousemove", mouseMove)
+
+        return () => {
+            window.removeEventListener("mousemove", mouseMove)
+        }
+    })
+
     return (
         <div className="cursor"></div>
     )
