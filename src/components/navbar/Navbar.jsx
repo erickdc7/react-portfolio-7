a9d6ba05c67a05fb2666c558247d408bed8c9b16
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-11 12:54:04 -0500

    add styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
@@ -7,7 +8,13 @@ const Navbar = () => {
       {/* Sidebar */}
       <Sidebar />
       <div className="wrapper">
-        <span>Lama Dev</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Lama Dev
+        </motion.span>
         <div className="social">
           <a href="#">
             <img src="/facebook.png" alt="" />
