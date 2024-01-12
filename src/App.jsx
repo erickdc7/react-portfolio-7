e04d98dc893c8586fb6fe9636410b75c35c9c2c3
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-01-12 12:41:08 -0500

    add comments

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,13 @@ const App = () => {
       <section id="Services">
         <Parallax type="services" />
       </section>
-      {/*  <section>
+      <section>
         <Services />
       </section>
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
-      <Portfolio />
+      {/* <Portfolio />
       <section id="Contact">
         <Contact />
       </section> */}
